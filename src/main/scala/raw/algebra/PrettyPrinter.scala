242a2f419c969ab321b62ce4eb99babee37a561f
Miguel Branco <msb@spacebase.org>
2015-02-12 11:37:46 +0100

    Beautify Algebra pretty printer

--- src/main/scala/raw/algebra/PrettyPrinter.scala
+++ src/main/scala/raw/algebra/PrettyPrinter.scala
@@ -6,20 +6,19 @@ package algebra
 object AlgebraPrettyPrinter extends PrettyPrinter {
 
   import Algebra._
-  import Expressions._
 
   def pretty(n: AlgebraNode): String =
     super.pretty(show(n))
 
   def show(a: AlgebraNode): Doc = a match {
-    case Scan(name)                  => "scan" <+> parens(text(name))
-    case Reduce(m, e, p, child)     => "reduce" <+> monoid(m) <+> ExpressionPrettyPrinter.pretty(e) <+> ExpressionPrettyPrinter.pretty(p) <@> nest(show(child))
-    case Nest(m, e, f, p, g, child) => "nest" <+> monoid(m) <+> ExpressionPrettyPrinter.pretty(e) <+> ExpressionPrettyPrinter.pretty(f) <+> ExpressionPrettyPrinter.pretty(p) <+> ExpressionPrettyPrinter.pretty(g) <@> nest(show(child))
-    case Select(p, child)           => "select" <+> ExpressionPrettyPrinter.pretty(p) <@> nest(show(child))
-    case Join(p, left, right)       => "join" <+> ExpressionPrettyPrinter.pretty(p) <@> nest(show(left)) <@> nest(show(right))
-    case Unnest(path, pred, child)        => "unnest" <+> ExpressionPrettyPrinter.pretty(path) <+> ExpressionPrettyPrinter.pretty(pred) <@> nest(show(child))
-    case OuterJoin(p, left, right)  => "outer_join" <+> ExpressionPrettyPrinter.pretty(p) <@> nest(show(left)) <@> nest(show(right))
-    case OuterUnnest(path, pred, child)   => "outer_unnest" <+> ExpressionPrettyPrinter.pretty(path) <+> ExpressionPrettyPrinter.pretty(pred) <@> nest(show(child))
-    case Merge(m, left, right)       => "merge" <+> monoid(m) <@> nest(show(left)) <@> nest(show(right))
+    case Scan(name)                     => "scan" <+> parens(text(name))
+    case Reduce(m, e, p, child)         => "reduce" <+> monoid(m) <+> ExpressionPrettyPrinter.pretty(e) <+> ExpressionPrettyPrinter.pretty(p) <@> nest(show(child))
+    case Nest(m, e, f, p, g, child)     => "nest" <+> monoid(m) <+> ExpressionPrettyPrinter.pretty(e) <+> ExpressionPrettyPrinter.pretty(f) <+> ExpressionPrettyPrinter.pretty(p) <+> ExpressionPrettyPrinter.pretty(g) <@> nest(show(child))
+    case Select(p, child)               => "select" <+> ExpressionPrettyPrinter.pretty(p) <@> nest(show(child))
+    case Join(p, left, right)           => "join" <+> ExpressionPrettyPrinter.pretty(p) <@> nest(show(left)) <@> nest(show(right))
+    case Unnest(path, pred, child)      => "unnest" <+> ExpressionPrettyPrinter.pretty(path) <+> ExpressionPrettyPrinter.pretty(pred) <@> nest(show(child))
+    case OuterJoin(p, left, right)      => "outer_join" <+> ExpressionPrettyPrinter.pretty(p) <@> nest(show(left)) <@> nest(show(right))
+    case OuterUnnest(path, pred, child) => "outer_unnest" <+> ExpressionPrettyPrinter.pretty(path) <+> ExpressionPrettyPrinter.pretty(pred) <@> nest(show(child))
+    case Merge(m, left, right)          => "merge" <+> monoid(m) <@> nest(show(left)) <@> nest(show(right))
   }
 }
