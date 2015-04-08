552ff4d2d0716d12e6cf6f5ba0932f343b84310f
Miguel Branco <msb@spacebase.org>
2015-04-08 17:03:00 +0200

    Removed ordering from user-defined case classes.

--- executor/src/test/scala/raw/csv/Entities.scala
+++ executor/src/test/scala/raw/csv/Entities.scala
@@ -2,25 +2,11 @@ package raw.csv
 
 import raw.util.CSVParser
 
-import scala.math.Ordered.orderingToOrdered
+case class Student(name: String, birthYear: Int, office: String, department: String)
 
-/* About Ordered[] using tuples:
- * http://stackoverflow.com/questions/19345030/easy-idiomatic-way-to-define-ordering-for-a-simple-case-class
- */
-case class Student(name: String, birthYear: Int, office: String, department: String) extends Ordered[Student] {
-  override def compare(that: Student): Int =
-    (name, birthYear, office, department).compare(that.name, that.birthYear, that.office, that.department)
-}
+case class Professor(name: String, office: String)
 
-case class Professor(name: String, office: String) extends Ordered[Professor] {
-  override def compare(that: Professor): Int =
-    (name, office).compare(that.name, that.office)
-}
-
-case class Department(name: String, discipline: String, prof: String) extends Ordered[Department] {
-  override def compare(that: Department): Int =
-    (name, discipline, prof).compare(that.name, that.discipline, that.prof)
-}
+case class Department(name: String, discipline: String, prof: String)
 
 object ReferenceTestData {
   val students: List[Student] = CSVParser("data/students.csv", l => Student(l(0), l(1).toInt, l(2), l(3)))
