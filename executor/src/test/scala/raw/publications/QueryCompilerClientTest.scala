2bcac1e0f2c5148042a1d651206150fe0426faa5
Nuno Santos <nfssantos@gmail.com>
2015-06-22 15:28:13 +0200

    Functional implementation of nest and unnest in Spark executor

--- executor/src/test/scala/raw/publications/QueryCompilerClientTest.scala
+++ executor/src/test/scala/raw/publications/QueryCompilerClientTest.scala
@@ -4,6 +4,7 @@ import com.google.common.collect.ImmutableMultiset
 import org.apache.spark.rdd.RDD
 import raw.algebra.LogicalAlgebra.LogicalAlgebraNode
 import raw.algebra.LogicalAlgebraPrettyPrinter
+import raw.csv.Student
 import raw.{RawQuery, rawQueryAnnotation}
 
 //@rawQueryAnnotation
@@ -13,12 +14,19 @@ import raw.{RawQuery, rawQueryAnnotation}
 
 @rawQueryAnnotation
 //class Query1(val authors: RDD[Author], val publications: RDD[Publication]) extends RawQuery {
-class Query1(val authors: RDD[Author]) extends RawQuery {
-//    val oql = """select title, count(partition) as n from authors A group by title: A.title"""
-//    val oql = """select P from publications P where "particle detectors" in P.controlledterms"""
-//  val oql = """select a.title, a.name from authors a where a.title = "PhD""""
-  val oql = """select (select year from partition)as years,title from authors A group by title: A.title"""
-//val oql = """select distinct title, (select distinct A from partition) as people from authors A group by title: A.title"""
+class Query1(val authors: RDD[Author], val publications: RDD[Publication]) extends RawQuery {
+  //    val oql = """select title, count(partition) as n from authors A group by title: A.title"""
+  //    val oql = """select P from publications P where "particle detectors" in P.controlledterms"""
+  //  val oql = """select a.title, a.name from authors a where a.title = "PhD""""
+  //  val oql = """select title, (select distinct year from partition) as years from authors A group by title: A.title"""
+  //  val oql = """select distinct title, (select distinct A from partition) as people from authors A group by title: A.title"""
+
+  // joins
+  //  val oql = """select a, b from authors a, authors b where a.year = b.year"""
+
+  // joins between tables
+//  val oql = """select distinct author, (select P from partition) as articles from publications P, P.authors A where P.title = "titletralala" and A = P.title group by author: A"""
+  val oql = """select author, (select P from partition) as articles from publications P, P.authors A group by author: A"""
 }
 
 class QueryCompilerClientTest extends AbstractSparkPublicationsTest {
@@ -35,8 +43,8 @@ class QueryCompilerClientTest extends AbstractSparkPublicationsTest {
   //  }
 
   test("simple oql") {
-//    val result: ImmutableMultiset[Author] = (new Query1(authorsRDD).computeResult).asInstanceOf[ImmutableMultiset[Author]]
-    val result = (new Query1(authorsRDD).computeResult)
+    //    val result: ImmutableMultiset[Author] = (new Query1(authorsRDD).computeResult).asInstanceOf[ImmutableMultiset[Author]]
+    val result = (new Query1(authorsRDD, publicationsRDD).computeResult).asInstanceOf[ImmutableMultiset[_]]
     logger.info("Result: " + result)
   }
 }
