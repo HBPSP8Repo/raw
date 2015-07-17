e3a9a2c15e9277a0f4f25db3c1861523fb66606e
Benjamin Gaidioz <benjamin.gaidioz@epfl.ch>
2015-07-17 15:37:58 +0200

    fix typos in docker script

--- scripts/docker-ldb-build-refresh-ldbrepo.sh
+++ scripts/docker-ldb-build-refresh-ldbrepo.sh
@@ -2,5 +2,5 @@
 # Figure out where RAW is installed
 RAW_ROOT="$(cd "`dirname "$0"`"/..; pwd)"
 
-touch.exe ${RAW_ROOT}/executor/src/test/docker/ldb/ldb-install.sh
-${RAW_ROOT}/bin/docker-ldb-build.sh
+touch ${RAW_ROOT}/executor/src/test/docker/ldb/ldb-install.sh
+${RAW_ROOT}/scripts/docker-ldb-build.sh
