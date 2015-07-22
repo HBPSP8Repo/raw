6389bd68a0e230ff06e7edc2f8289d920305a2a7
Nuno Santos <nfssantos@gmail.com>
2015-07-22 12:28:37 +0200

    Added more queries to performance tests. Other minor improvements to perf tests driver.

--- executor/src/main/scala/raw/perf/PerfMain.scala
+++ executor/src/main/scala/raw/perf/PerfMain.scala
@@ -1,8 +1,9 @@
 package raw.perf
 
+import java.io.PrintStream
 import java.net.{URI, URL}
 import java.nio.charset.StandardCharsets
-import java.nio.file.{Files, Paths}
+import java.nio.file.{Files, Path, Paths}
 import java.util.concurrent.TimeUnit
 
 import com.google.common.base.Stopwatch
@@ -12,7 +13,7 @@ import org.apache.commons.math3.stat.descriptive.DescriptiveStatistics
 import org.apache.spark.SparkContext
 import org.apache.spark.sql.hive.HiveContext
 import org.apache.spark.sql.{DataFrame, Row}
-import org.rogach.scallop.{ScallopConf, ScallopOption}
+import org.rogach.scallop.ScallopConf
 import raw.RawQuery
 import raw.datasets.publications.Publications
 import raw.datasets.{AccessPath, Dataset}
@@ -24,11 +25,16 @@ object Queries {
 
   case class Query(name: String, oql: String, hql: String)
 
+  def formatQuery(query: String): String = {
+    // Remove the whitespace at the start of each line
+    query.trim.replaceAll("\\n\\s+", "\n")
+  }
+
   def readQueries(file: String): Seq[Query] = {
     val x = scala.xml.XML.loadFile(file)
     val queries = (x \ "query")
       .map(queryNode =>
-      Query((queryNode \ "@name").text.trim, (queryNode \ "oql").text.trim, (queryNode \ "hql").text.trim))
+      Query((queryNode \ "@name").text.trim, formatQuery((queryNode \ "oql").text), formatQuery((queryNode \ "hql").text)))
     queries.toSeq
   }
 
@@ -77,19 +83,18 @@ object PerfMain extends StrictLogging with ResultConverter {
     df
   }
 
-
   def statsToString(stats: DescriptiveStatistics): String = {
     f"${stats.getMean}%6.3f, StdDev: ${stats.getStandardDeviation}%6.3f, Min: ${stats.getMin}%6.3f, Max: ${stats.getMax}%6.3f"
   }
 
-  var hqlCount = 0
+  var hqlCount = -1
 
   def nextHQLFilename(): String = {
     hqlCount += 1
     s"HQLQuery$hqlCount.txt"
   }
 
-  var oqlCount = 0
+  var oqlCount = -1
 
   def nextOQLFilename(): String = {
     oqlCount += 1
@@ -100,102 +105,119 @@ object PerfMain extends StrictLogging with ResultConverter {
     stats.getValues.map(d => f"$d%2.3f").mkString(", ")
   }
 
-  def timeHQL(hql: String, repeats: Int, outDir: String): Option[DescriptiveStatistics] = {
-    logger.info(s"**************** Query:\n$hql")
-
-    val p = Paths.get(outDir, nextHQLFilename())
-    val br = Files.newBufferedWriter(p, StandardCharsets.UTF_8)
-    br.write(s"$hql\n\n")
-
-    val execTimes = new DescriptiveStatistics()
-    var resDF: DataFrame = null
-    var res: Array[Row] = null
-    for (i <- 1 to repeats) {
-      val clock = Stopwatch.createStarted()
-      resDF = sqlContext.sql(hql)
-      res = resDF.collect()
-      execTimes.addValue(clock.elapsed(TimeUnit.MILLISECONDS) / 1000.0)
-    }
-
-    resDF.explain()
-
-    logger.info(s"Execution: ${valuesToString(execTimes)}")
-    br.write(s"Execution: ${valuesToString(execTimes)}\n\n")
-
-    logger.info(s"Execution: ${statsToString(execTimes)}")
-    br.write(s"Execution: ${statsToString(execTimes)}\n\n")
-
-    br.write(s"Query result:\n${res.mkString("\n")}\n")
-    br.close()
-
-    Some(execTimes)
-  }
-
-  case class OQLResults(compile: DescriptiveStatistics, exec: DescriptiveStatistics, total: DescriptiveStatistics)
-
-  def timeOQL(oql: String, repeats: Int, outDir: String, comp: QueryCompilerClient, accessPaths: List[AccessPath[_]]): Option[OQLResults] = {
-    logger.info(s"**************** Query:\n$oql")
-
-    val p = Paths.get(outDir, nextOQLFilename())
-    val br = Files.newBufferedWriter(p, StandardCharsets.UTF_8)
-
-    br.write(s"$oql\n\n")
-
-    val compileTimes = new DescriptiveStatistics()
-    val execTimes = new DescriptiveStatistics()
-    val totals = new DescriptiveStatistics()
-
-    var res: Any = null
-
-    for (i <- 1 to repeats) {
-      val clock = Stopwatch.createStarted()
-      val result = comp.compileOQL(oql, accessPaths) match {
-        case Left(error) => {
-          logger.warn(s"Error compiling query: $oql. Error: $error")
-          return None
-        }
-        case Right(query: RawQuery) => {
-          val compileTime = clock.elapsed(TimeUnit.MILLISECONDS) / 1000.0
-          compileTimes.addValue(compileTime)
-          clock.reset().start()
-          res = query.computeResult
-          val execTime = clock.elapsed(TimeUnit.MILLISECONDS) / 1000.0
-          execTimes.addValue(execTime)
-          totals.addValue(compileTime + execTime)
-        }
-      }
-    }
-
-    logger.info(s"Compile: ${valuesToString(compileTimes)}")
-    br.write(s"Compile: ${valuesToString(compileTimes)}\n")
-    logger.info(s"  Summary: ${statsToString(compileTimes)}")
-    br.write(s"  Summary: ${statsToString(compileTimes)}\n")
-
-    logger.info(s"Execution: ${valuesToString(execTimes)}")
-    br.write(s"Execution: ${valuesToString(execTimes)}\n\n")
-    logger.info(s"Summary: ${statsToString(execTimes)}")
-    br.write(s"Summary: ${statsToString(execTimes)}\n\n")
-
-    br.write(s"Query result:\n${convertToJson(res)}\n")
-    br.close()
-
-    Some(OQLResults(compileTimes, execTimes, totals))
-  }
-
   def main(args: Array[String]) {
     object Conf extends ScallopConf(args) {
       banner("Performance test driver")
-      val repeats: ScallopOption[Int] = opt[Int]("repeats", default = Some(5), short = 'r')
-      val outputDir: ScallopOption[String] = opt[String]("outputDir", default = Some(Paths.get(System.getProperty("java.io.tmpdir"), "raw-perf-test").toString), short = 'o')
-      val runQueryType: ScallopOption[String] = opt[String]("runTypes", default = Some("all"), short = 't')
-      val dataset: ScallopOption[String] = opt[String]("dataset", default = Some("publications"), short = 'd', descr = "Possible choices: publications, publicationsLarge")
-      val queryFile: ScallopOption[String] = trailArg[String](required = true)
+      val repeats = opt[Int]("repeats", default = Some(5), short = 'r')
+      val outputDir = opt[String]("outputDir", default = Some(Paths.get(System.getProperty("java.io.tmpdir"), "raw-perf-test").toString), short = 'o')
+      val runQueryType = opt[String]("runTypes", default = Some("all"), short = 't')
+      val dataset = opt[String]("dataset", default = Some("publications"), short = 'd', descr = "Possible choices: publications, publicationsLarge")
+      val saveResults = opt[Boolean]("save-results", default = Some(false), short = 's', descr = "Save the results of the query")
+      val queryFile = trailArg[String](required = true)
     }
+
+    def timeHQL(hql: String): Option[DescriptiveStatistics] = {
+      logger.info(s"Testing query:\n$hql")
+
+      val p = Paths.get(Conf.outputDir(), nextHQLFilename())
+      val br = new PrintStream(Files.newOutputStream(p))
+      br.println(s"$hql\n")
+
+      val execTimes = new DescriptiveStatistics()
+      var resDF: DataFrame = null
+      var res: Array[Row] = null
+      for (i <- 1 to Conf.repeats()) {
+        val clock = Stopwatch.createStarted()
+        resDF = sqlContext.sql(hql)
+        res = resDF.collect()
+        execTimes.addValue(clock.elapsed(TimeUnit.MILLISECONDS) / 1000.0)
+      }
+
+      Console.withOut(br) {
+        resDF.explain()
+      }
+      br.println()
+
+      br.println(s"Execution : ${statsToString(execTimes)}")
+      br.println(s"Samples   : ${valuesToString(execTimes)}\n")
+
+      if (Conf.saveResults()) {
+        br.println(s"Query result:\n${res.mkString("\n")}")
+      }
+      br.close()
+
+      Some(execTimes)
+    }
+
+    case class OQLResults(compile: DescriptiveStatistics, exec: DescriptiveStatistics, total: DescriptiveStatistics)
+
+    def timeOQL(oql: String, comp: QueryCompilerClient, accessPaths: List[AccessPath[_]]): Option[OQLResults] = {
+      logger.info(s"**************** Query:\n$oql")
+
+      val p = Paths.get(Conf.outputDir(), nextOQLFilename())
+      val br = new PrintStream(Files.newOutputStream(p))
+      br.println(s"$oql\n")
+
+      val compileTimes = new DescriptiveStatistics()
+      val execTimes = new DescriptiveStatistics()
+      val totals = new DescriptiveStatistics()
+
+      var res: Any = null
+
+      for (i <- 1 to Conf.repeats()) {
+        val clock = Stopwatch.createStarted()
+        val result = comp.compileOQL(oql, accessPaths) match {
+          case Left(error) => {
+            logger.warn(s"Error compiling query: $oql. Error: $error")
+            return None
+          }
+          case Right(query: RawQuery) => {
+            val compileTime = clock.elapsed(TimeUnit.MILLISECONDS) / 1000.0
+            compileTimes.addValue(compileTime)
+            clock.reset().start()
+            res = query.computeResult
+            val execTime = clock.elapsed(TimeUnit.MILLISECONDS) / 1000.0
+            execTimes.addValue(execTime)
+            totals.addValue(compileTime + execTime)
+          }
+        }
+      }
+
+      br.println(s"Compile  : ${statsToString(compileTimes)}")
+      br.println(s"Samples  : ${valuesToString(execTimes)}\n")
+
+      br.println(s"Execution: ${statsToString(execTimes)}")
+      br.println(s"Samples  : ${valuesToString(execTimes)}\n")
+
+      br.println(s"Total    : ${statsToString(totals)}")
+      br.println(s"Samples  : ${valuesToString(totals)}\n")
+
+      if (Conf.saveResults()) {
+        br.println(s"Query result:\n${convertToJson(res)}")
+      }
+      br.close()
+
+      Some(OQLResults(compileTimes, execTimes, totals))
+    }
+
+    def writeSparkConfiguration(path: Path) = {
+      val sparkConfigBW = Files.newBufferedWriter(path, StandardCharsets.UTF_8)
+      sparkConfigBW.write(sc.getConf.getAll.sorted.mkString("\n"))
+      sparkConfigBW.newLine()
+      sparkConfigBW.write(sqlContext
+        .getAllConfs
+        .map(t => s"${t._1} ->  ${t._2}")
+        .mkString("\n"))
+      sparkConfigBW.close()
+    }
+
+
     val runOQL = Conf.runQueryType() == "all" || Conf.runQueryType() == "oql"
     val runHQL = Conf.runQueryType() == "all" || Conf.runQueryType() == "hql"
 
-    val queries = Queries.readQueries(Conf.queryFile())
+    writeSparkConfiguration(Paths.get(Conf.outputDir(), "sparkConfig.txt"))
 
+    val queries = Queries.readQueries(Conf.queryFile())
     val resDir = Paths.get(Conf.outputDir())
     if (!Files.isDirectory(resDir)) {
       logger.info(s"Creating results directory: $resDir")
@@ -204,15 +226,18 @@ object PerfMain extends StrictLogging with ResultConverter {
 
     val hqlResults = new ArrayBuffer[String]()
     if (runHQL) {
+      // Load and register as tables the data sources
       readJson("data/publications/authors.json", "authors")
       Conf.dataset() match {
         case "publications" => readJson("data/publications/publications.json", "publications")
         case "publicationsLarge" => readJson("data/publications/publicationsLarge.json", "publications")
         case _ => throw new IllegalArgumentException("Invalid dataset")
       }
+
+      // Execute the tests
       for (q <- queries) {
         if (q.hql != "") {
-          timeHQL(q.hql, Conf.repeats(), Conf.outputDir()) match {
+          timeHQL(q.hql) match {
             case Some(qr) => hqlResults += s"    Total:   ${statsToString(qr)}"
             case None => hqlResults += "Fail"
           }
@@ -232,7 +257,7 @@ object PerfMain extends StrictLogging with ResultConverter {
       setDockerHost()
       for (q <- queries) {
         if (q.oql != "") {
-          timeOQL(q.oql, Conf.repeats(), Conf.outputDir(), comp, accessPaths) match {
+          timeOQL(q.oql, comp, accessPaths) match {
             case Some(queryRes) => oqlResults +=
               s"    Compile: ${statsToString(queryRes.compile)}\n" +
                 s"    Exec:    ${statsToString(queryRes.exec)}\n" +
@@ -245,18 +270,19 @@ object PerfMain extends StrictLogging with ResultConverter {
     }
 
     val p = Paths.get(Conf.outputDir(), "summary.txt")
-    val br = Files.newBufferedWriter(p, StandardCharsets.UTF_8)
-    br.write(s"Configuration:\n${Conf.summary}\n")
+    val summaryFileBW = new PrintStream(Files.newOutputStream(p))
+    summaryFileBW.println(s"Configuration:\n${Conf.summary}")
+    // Write the results to the summary file
     for (i <- 0 until queries.size) {
-      br.write(s"Query: ${queries(i).name}\n")
+      summaryFileBW.println(s"Query $i:  ${queries(i).name}")
       if (!oqlResults.isEmpty) {
-        br.write(s"  OQL:\n${oqlResults(i)}\n")
+        summaryFileBW.println(s"  OQL:\n${oqlResults(i)}")
       }
       if (!hqlResults.isEmpty) {
-        br.write(s"  HQL:\n${hqlResults(i)}\n")
+        summaryFileBW.println(s"  HQL:\n${hqlResults(i)}")
       }
-      br.newLine()
+      summaryFileBW.println()
     }
-    br.close()
+    summaryFileBW.close()
   }
 }
