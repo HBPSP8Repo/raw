6d5d46eca200821d37c6e2416f8c143382a571c5
Nuno Santos <nfssantos@gmail.com>
2015-07-27 10:32:39 +0200

    Use implicit parameter for classtag argument of AccessPaths. Added documentation.

--- executor/src/main/scala/raw/datasets/Dataset.scala
+++ executor/src/main/scala/raw/datasets/Dataset.scala
@@ -6,28 +6,22 @@ import raw.executionserver.{DefaultSparkConfiguration, JsonLoader}
 
 import scala.reflect._
 
-case class AccessPath[T](name: String, path: RDD[T], tag: ClassTag[T])
+/**
+ * If the type parameter used in the access path contains custom subtypes, they must be in the same package as the top
+ * level types. This is because the code generator will only add imports for the packages of the top-level types.
+ *
+ * @param name The identifier of the access path, used to generate the query code.
+ * @param path The actual instance of the access path, used to execute the query.
+ * @param tag Type information of the access path. Used to generate the query code.
+ */
+// Note: name and tag are execute for generating the code, while path is for creating an instance of the query.
+// The compilation and instantiation should be separated. This would allow caching the queries and executing the
+// same query with different access paths.
+case class AccessPath[T: ClassTag](name: String, path: RDD[T])(implicit val tag: ClassTag[T])
 
-class Dataset[T:ClassTag](name:String, file:String, sc:SparkContext) {
+class Dataset[T: ClassTag](name: String, file: String, sc: SparkContext) {
   val data: List[T] = JsonLoader.load[T](file)
   val rdd = DefaultSparkConfiguration.newRDDFromJSON[T](data, sc)
-  val accessPath: AccessPath[_] = AccessPath(name, rdd, classTag[T])
+  val accessPath: AccessPath[_] = AccessPath[T](name, rdd)
 }
 
-
-//package raw.datasets
-//
-//import org.apache.spark.SparkContext
-//import org.apache.spark.rdd.RDD
-//import raw.executionserver.{DefaultSparkConfiguration, JsonLoader}
-//
-//import scala.reflect.runtime.universe._
-//
-//case class AccessPath[T](name: String, path: RDD[T], tag: TypeTag[T])
-//
-//class Dataset[T: TypeTag](name: String, file: String, sc: SparkContext) {
-//  val data: List[T] = JsonLoader.load[T](file)
-//  val rdd = DefaultSparkConfiguration.newRDDFromJSON[T](data, sc)
-//  val accessPath: AccessPath[_] = AccessPath(name, rdd, typeTag[T])
-//}
-//
--- executor/src/main/scala/raw/perf/QueryCompilerClient.scala
+++ executor/src/main/scala/raw/perf/QueryCompilerClient.scala
@@ -12,7 +12,6 @@ import raw.RawQuery
 import raw.datasets.AccessPath
 import raw.executionserver.RawMutableURLClassLoader
 
-import scala.reflect.ClassTag
 import scala.tools.nsc.reporters.StoreReporter
 import scala.tools.nsc.{Global, Settings}
 
@@ -76,16 +75,24 @@ class QueryCompilerClient(val rawClassloader: RawMutableURLClassLoader, outputDi
     compile("plan", plan, accessPaths)
   }
 
-  private[this] def compile(queryFieldName: String, plan: String, accessPaths: List[AccessPath[_]]): Either[String, RawQuery] = {
+  private[this] def compile(queryFieldName: String, query: String, accessPaths: List[AccessPath[_]]): Either[String, RawQuery] = {
     //    logger.info("Access paths: " + accessPaths)
     val queryName = newClassName()
     val aps: List[String] = accessPaths.map(ap => ap.tag.toString())
     logger.info(s"Access paths: $aps")
+
+    /* For every top level type argument of the access path, import the containing package. The is, for the following
+     * access paths: RDD[raw.Publications], RDD[raw.patients.Patient], generate "import raw._" and "import raw.patients._"
+     *
+     * NOTE: this does not check nested types, that is, if Patient contains references to a Diagnostic instance,
+     * it will only import the package of Patient. Therefore, any nested types should be in the same package as the
+     * top level type. This limitation can be eliminated by using TypeTags instead of ClassTags in the AccessPath class
+     * and by recursively scanning the full type of the access path.
+     */
     val imports = aps.map(ap => ap.lastIndexOf(".") match {
       case -1 => return Left(s"Case classes in access paths should not be at top level package: $ap")
       case i: Int => "import " + ap.substring(0, i + 1) + "_"
     }).toSet.mkString("\n")
-    logger.info(s"Packages: $imports")
 
     //    val imports = accessPaths.map(ap => s"import ${ap.tag.toString()}").mkString("\n")
     val args = accessPaths.map(ap => s"${ap.name}: RDD[${ap.tag.runtimeClass.getSimpleName}]").mkString(", ")
@@ -101,7 +108,7 @@ $imports
 class $queryName($args) extends RawQuery {
   val $queryFieldName =
   \"\"\"
-  $plan
+  $query
   \"\"\"
 }
 """
@@ -122,14 +129,16 @@ class $queryName($args) extends RawQuery {
       return Left(message)
     }
 
-    // Load the main query class and run it.
+    // Load the main query class
     val queryClass = s"raw.query.$queryName"
+    logger.info("Creating new instance of: " + queryClass)
     val clazz = rawClassloader.loadClass(queryClass)
 
+    // Find the constructor
     val ctorTypeArgs = List.fill(accessPaths.size)(classOf[RDD[_]])
     val ctor = clazz.getConstructor(ctorTypeArgs: _*)
-    logger.info("Invoking constructor: " + ctor)
 
+    // Create an instance of the query using the rdd instance given in the access paths.
     val ctorArgs = accessPaths.map(ap => ap.path)
     Right(ctor.newInstance(ctorArgs: _*).asInstanceOf[RawQuery])
   }
