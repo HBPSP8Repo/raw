93040ac02a039256da8dc6bfb1be834e101a1c91
Nuno Santos <nfssantos@gmail.com>
2015-09-08 15:31:27 +0200

    Use standard Linux installation directories for Docker images.

--- executor/src/test/docker/ldb/shore-mt-install.sh
+++ executor/src/test/docker/ldb/shore-mt-install.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 set -e
 
-cd /raw
+cd /tmp
 git clone git://github.com/raw-db/shore-mt.git
 cd shore-mt
 ./bootstrap
-./configure --enable-dbgsymbols --prefix=/raw/shoremt
+./configure --enable-dbgsymbols --prefix=/opt/shoremt
 make -j4
 make install
 cd ..
