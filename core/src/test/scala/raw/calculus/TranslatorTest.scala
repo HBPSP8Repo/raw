0f4150a6a6ed504ec0653c07a97d03e34468036a
Benjamin Gaidioz <benjamin.gaidioz@epfl.ch>
2015-09-25 15:08:38 +0200

    handling of distinct, wrote a couple more test cases in translator

--- core/src/test/scala/raw/calculus/TranslatorTest.scala
+++ core/src/test/scala/raw/calculus/TranslatorTest.scala
@@ -27,49 +27,53 @@ class TranslatorTest extends FunTest {
     compare(
       process(
         """select s.name from students s""", TestWorlds.professors_students),
-        """for ($0 <- students) yield bag $0.name""")
+        """for ($0 <- to_bag(students)) yield bag $0.name""")
   }
 
   test("select s.name from students s where s.age > 10") {
     compare(
       process(
         """select s.name from students s where s.age > 10""", TestWorlds.professors_students),
-      """for ($0 <- students; $0.age > 10) yield bag $0.name""")
+      """for ($0 <- to_bag(students); $0.age > 10) yield bag $0.name""")
   }
 
   test("select s.age/10, partition from students s group by s.age/10") {
     compare(
       process(
         """select s.age/10, partition from students s group by s.age/10""", TestWorlds.professors_students),
-        """for ($0 <- students) yield bag (_1: $0.age / 10, _2: for ($1 <- students; $0.age / 10 = $1.age / 10) yield bag $1)""")
+        """for ($0 <- to_bag(students)) yield bag (_1: $0.age / 10, _2: for ($1 <- to_bag(students); $0.age / 10 = $1.age / 10) yield bag $1)""")
   }
 
   test("select s.age/10 as decade, partition as students from students s group by s.age/10") {
     compare(
       process(
         """select s.age/10 as decade, partition as students from students s group by s.age/10""", TestWorlds.professors_students),
-        """for ($0 <- students) yield bag (decade: $0.age / 10, students: for ($1 <- students; $0.age / 10 = $1.age / 10) yield bag $1)""")
+        """for ($0 <- to_bag(students)) yield bag (decade: $0.age / 10, students: for ($1 <- to_bag(students); $0.age / 10 = $1.age / 10) yield bag $1)""")
   }
 
   test("select s.age/10 as decade, (select s.name from partition s) as names from students s group by s.age/10") {
     compare(
       process(
         """select s.age/10 as decade, (select s.name from partition s) as names from students s group by s.age/10""", TestWorlds.professors_students),
-        """for ($0 <- students) yield bag (decade: $0.age / 10, names: for ($2 <- for ($1 <- students; $0.age / 10 = $1.age / 10) yield bag $1) yield bag $2.name)""")
+        """for ($0 <- to_bag(students)) yield bag (decade: $0.age / 10, names: for ($2 <- to_bag(for ($1 <- to_bag(students); $0.age / 10 = $1.age / 10) yield bag $1)) yield bag $2.name)""")
   }
 
   test("select s.age, (select s.name, partition from partition s group by s.name) as names from students s group by s.age") {
     compare(
       process(
         """select s.age, (select s.name, partition from partition s group by s.name) as names from students s group by s.age""", TestWorlds.professors_students),
-        """for ($0 <- students) yield bag (_1: $0.age, names: for ($1 <- for ($2 <- students; $0.age = $2.age) yield bag $2) yield bag (_1: $1.name, _2: for ($3 <- for ($2 <- students; $0.age = $2.age) yield bag $2; $1.name = $3.name) yield bag $3))""")
+        """for ($0 <- to_bag(students)) yield bag
+              (_1:    $0.age,
+               names: for ($1 <- to_bag(for ($2 <- to_bag(students); $0.age = $2.age) yield bag $2)) yield bag
+                            (_1: $1.name,
+                             _2: for ($3 <- to_bag(for ($2 <- to_bag(students); $0.age = $2.age) yield bag $2); $1.name = $3.name) yield bag $3))""")
   }
 
   test("select A from authors A") {
     compare(
       process(
         "select A from authors A", TestWorlds.publications),
-        "for ($0 <- authors) yield bag $0"
+        "for ($0 <- to_bag(authors)) yield bag $0"
       )
   }
 
@@ -77,7 +81,7 @@ class TranslatorTest extends FunTest {
     compare(
       process(
         "select P from publications P", TestWorlds.publications),
-        "for ($0 <- publications) yield bag $0"
+        "for ($0 <- to_bag(publications)) yield bag $0"
     )
   }
 
@@ -85,7 +89,7 @@ class TranslatorTest extends FunTest {
     compare(
       process(
         "select A.title, count(partition) as n from authors A group by A.title", TestWorlds.publications),
-        """for ($0 <- authors) yield bag (_1: $0.title, n: \$1 -> for ($2 <- $1) yield sum 1(for ($3 <- authors; $0.title = $3.title) yield bag $3))"""
+        """for ($0 <- to_bag(authors)) yield bag (_1: $0.title, n: \$1 -> for ($2 <- $1) yield sum 1(for ($3 <- to_bag(authors); $0.title = $3.title) yield bag $3))"""
     )
   }
 
@@ -93,7 +97,7 @@ class TranslatorTest extends FunTest {
     compare(
       process(
         "select A.title, partition as people from authors A group by A.title", TestWorlds.publications),
-      """for ($0 <- authors) yield bag (_1: $0.title, people: for ($3 <- authors; $0.title = $3.title) yield bag $3)"""
+      """for ($0 <- to_bag(authors)) yield bag (_1: $0.title, people: for ($3 <- to_bag(authors); $0.title = $3.title) yield bag $3)"""
     )
   }
 
@@ -101,32 +105,31 @@ class TranslatorTest extends FunTest {
     compare(
       process(
         "select A.title, (select A.year from A in partition) as years from authors A group by A.title", TestWorlds.publications),
-      """for ($0 <- authors) yield bag (_1: $0.title, years: for ($3 <- for ($4 <- authors; $0.title = $4.title) yield bag $4) yield bag $3.year)"""
+      """for ($0 <- to_bag(authors)) yield bag (_1: $0.title, years: for ($3 <- to_bag(for ($4 <- to_bag(authors); $0.title = $4.title) yield bag $4)) yield bag $3.year)"""
     )
   }
 
   test("distinct leads to a set") {
     compare(
       process("select distinct A.title from A in authors", TestWorlds.publications),
-      "for ($0 <- authors) yield set $0"
+      "to_set(for ($0 <- to_bag(authors)) yield bag $0.title)"
     )
   }
 
-  ignore("select G.title, (select year: v, N: count(partition) from v in G.values group by year: v) as stats from ( select distinct title, (select year from partition) as values from authors A group by title: A.title) G") {
+  test("select G.title, (select year: v, N: count(partition) from v in G.values group by year: v) as stats from ( select distinct title, (select year from partition) as values from authors A group by title: A.title) G") {
     compare(
       process(
       """select G.title,
-               (select year: v.year,
+               (select distinct year: v.year,
                        N: count(partition)
                 from v in G.people
                 group by v.year) as stats
          from (
-               select distinct title: A.title, partition as people
+               select title: A.title, partition as people
                from authors A
                group by A.title) G""", TestWorlds.publications),
-      """for ($10 <-
-          for ($0 <- authors) yield set (_1: $0.title, people: for ($3 <- authors; $0.title = $3.title) yield bag $3.year)
-         ) yield bag (_1: $10.title, stats: for ($12 <- $10.years) yield bag (year: $12, N: count(partition)))
+      """for ($10 <- to_bag(for ($0 <- to_bag(authors)) yield bag (title: $0.title, people: for ($3 <- to_bag(authors); $0.title = $3.title) yield bag $3)))
+          yield bag (_1: $10.title, stats: to_set(for ($12 <- to_bag($10.people)) yield bag (year: $12.year, N: \$13 -> for ($14 <- $13) yield sum 1(for ($22 <- to_bag($10.people); $12.year = $22.year) yield bag $22))))
       """
     )
   }
