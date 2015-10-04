4c8562edb7455926f3636adfe46c4e458530001f
Miguel Branco <miguel@raw-labs.com>
2015-10-04 20:59:44 +0200

    Deleting dead code

--- core/src/main/scala/raw/calculus/ExpressionsDesugarer.scala
+++ core/src/main/scala/raw/calculus/ExpressionsDesugarer.scala
@@ -106,26 +106,4 @@ class ExpressionsDesugarer(val analyzer: SemanticAnalyzer) extends Attribution w
       os
   }
 
-  /** De-sugar anonymous records, e.g.
-    *   for ( <- students ) yield set ( age, for ( <- professors ) yield max age )
-    * becomes
-    *   for ( $0 <- students ) yield set ( $0.age, for ( $1 <- professors ) yield max $1.age )
-    */
-
-  // Generate unique IDs for Gens w/o pattern
-  private lazy val anonGenSymbol: Gen => Symbol = attr {
-    case Gen(None, _) => SymbolTable.next()
-  }
-
-  private lazy val anonRecords = rule[Exp] {
-    case idnExp: IdnExp if analyzer.lookupAttributeEntity(idnExp) != UnknownEntity() =>
-      analyzer.lookupAttributeEntity(idnExp) match {
-        case AttributeEntity(t, g, idx) => RecordProj(IdnExp(IdnUse(anonGenSymbol(g).idn)), s"_${idx + 1}")
-      }
-  }
-
-  private lazy val anonGens = rule[Gen] {
-    case g @ Gen(None, e) => Gen(Some(PatternIdn(IdnDef(anonGenSymbol(g).idn))), e)
-  }
-
 }
