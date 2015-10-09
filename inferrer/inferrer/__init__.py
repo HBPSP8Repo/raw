ddaa803d51a07bd98440b0a7766bcb1e2062bf38
cesar matos <cesar.matos@gmail.com>
2015-10-09 17:31:20 +0200

    added sample functions for csv and json

--- inferrer/inferrer/__init__.py
+++ inferrer/inferrer/__init__.py
@@ -1,15 +1,38 @@
 # Infer schema from a local file
+from splitstream import splitfile
 
 from .csv_inferrer import CSVInferrer
 from .json_inferrer import JSONInferrer
 
 
+def json_sample(path):
+
+    with f = open(path, 'r'):
+        s = f.read(500).lstrip() 
+        if s[0] != '[':
+            # check if another with block is needed 
+            return  open(path, 'r').read()
+        with f = open(path, 'r'):
+            gen = splitfile(f, format="json", startdepth=1)
+            try:
+                sample = next(gen)
+                return "[\n%s\n]" % sample
+            except StopIteration: 
+                # check if another with block is needed 
+               return  open(path, 'r').read()
+
+def csv_sample(path):
+    # check if this is the best way of doing it
+    N_LINES = 100
+    with f = open(path, 'r'):
+        lines = f.readlines(N_LINES);
+        return "".join(lines)
+    
 def from_local(name, path, file_type):
-    with open(path, 'r') as f:
-        if file_type == 'json':
-            inferrer = JSONInferrer(name, f.read())
-        elif file_type == 'csv':
-            inferrer = CSVInferrer(name, f.read())
-        else:
-            raise ValueError("Type not supported")
-        return inferrer.infer_type()
+    if file_type == 'json':
+        inferrer = JSONInferrer(name,  json_sample(path) )
+    elif file_type == 'csv':
+        inferrer = CSVInferrer(name, csv_sample())
+    else:
+        raise ValueError("Type not supported")
+    return inferrer.infer_type()
