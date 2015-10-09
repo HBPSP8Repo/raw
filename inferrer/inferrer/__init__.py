1b825cb93dea931040ab843540beff37b12f2c6b
cesar matos <cesar.matos@gmail.com>
2015-10-09 17:42:50 +0200

    commit before going home

--- inferrer/inferrer/__init__.py
+++ inferrer/inferrer/__init__.py
@@ -6,27 +6,26 @@ from .json_inferrer import JSONInferrer
 
 
 def json_sample(path):
-
-    with f = open(path, 'r'):
+    with open(path, 'r') as f:
         s = f.read(500).lstrip() 
-        if s[0] != '[':
+    if s[0] != '[':
+        # check if another with block is needed 
+        return  open(path, 'r').read()
+    with open(path, 'r') as f:
+        gen = splitfile(f, format="json", startdepth=1)
+        try:
+            sample = next(gen)
+            return "[\n%s\n]" % (sample)
+        except StopIteration: 
             # check if another with block is needed 
-            return  open(path, 'r').read()
-        with f = open(path, 'r'):
-            gen = splitfile(f, format="json", startdepth=1)
-            try:
-                sample = next(gen)
-                return "[\n%s\n]" % sample
-            except StopIteration: 
-                # check if another with block is needed 
-               return  open(path, 'r').read()
+           return  open(path, 'r').read()
 
 def csv_sample(path):
     # check if this is the best way of doing it
     N_LINES = 100
-    with f = open(path, 'r'):
+    with open(path, 'r') as f:
         lines = f.readlines(N_LINES);
-        return "".join(lines)
+    return "".join(lines)
     
 def from_local(name, path, file_type):
     if file_type == 'json':
